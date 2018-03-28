774e185d808e7d6f2c631744f9d888719ade144a
Aishwarya Sharma <ishans632@gmail.com>
2018-03-28 08:27:25 +0530

    Add mongodb to store search urls

--- ImageSearchAbstractionMicroservice/server.js
+++ ImageSearchAbstractionMicroservice/server.js
@@ -4,27 +4,75 @@ let https = require('https');
 let bodyParser = require('body-parser'); 
 let express = require('express'); 
 let url = require('url'); 
+let {MongoClient} = require('mongodb');
 
 let app = express(); 
 
 app.use(bodyParser.json()); 
-
-// storing the response_handler output into an array 
-let jsonResp = []; 
 
 // * after searchParams acts like a regex expr for 0 or more 
 // ? refers to query so req.query method must be used 
 // : refers to params so req.params must be used 
 
 
+const client_pass = "test"
+const client_user = "test"
+const client_url = `mongodb://test:test@ds125479.mlab.com:25479/imgsearch`;
+
+
+
+app.get('/latest/imagesearch/', (req, res) => {
+    MongoClient.connect(client_url, (err, client) => {
+        if(err){
+            console.log('Unable to connect to MongoDB server'); 
+            return; 
+        }
+
+        console.log('connected to mongodb server'); 
+        let db = client.db('imgsearch'); 
+
+        db.collection('img').find().limit(10).toArray((err, result)  => {
+            if(err){
+                console.log('Unable to fetch results'); 
+            }
+            res.send(JSON.stringify(result, undefined, 2));
+        }); 
+        
+        client.close;
+    }); 
+ 
+
+}); 
+
+
 app.get('/imagesearch/:searchParams*', (req, res) => {
     let { searchParams } = req.params; 
     let { offset } = req.query; 
 
-    // BING SEARCH WEB API KEY REQUIRED
+    MongoClient.connect(client_url, (err, client) => {
+        if(err){
+            console.log('Unable to connect to MongoDB server'); 
+            return; 
+        }
+
+        console.log('connected to mongodb server'); 
+        let db = client.db('imgsearch'); 
+
+        db.collection('img').insertOne({
+            "searchUrl" : req.get('host') + req.url,   
+        }, (err, result) => {
+            if(err){
+                return res.send('Unable to insert data'); 
+            }
+            console.log('Inserted the data into mongo'); 
+        });
+        
+        client.close;
+    }); 
+
+    
     // Replace the subscriptionKey string value with your valid subscription key.
     let subscriptionKey = '';
-	
     // Verify the endpoint URI.  At this writing, only one endpoint is used for Bing
     // search APIs.  In the future, regional endpoints may be available.  If you
     // encounter unexpected authorization errors, double-check this host against
@@ -50,7 +98,6 @@ app.get('/imagesearch/:searchParams*', (req, res) => {
                     "name" : body[i]["name"],
                     "image" : body[i]["contentUrl"], 
                     "thumnail" : body[i]["thumbnailUrl"], 
-                    "text" : body[i]["insightsMetadata"]["bestRepresentativeQuery"]["text"]
                 }); 
             }
 
@@ -89,9 +136,8 @@ app.get('/imagesearch/:searchParams*', (req, res) => {
         console.log('Invalid Bing Search API subscription key!');
         console.log('Please paste yours into the source code.');
     }
+
 }); 
-
-
 
 
 // Listening to PORT 3000 
